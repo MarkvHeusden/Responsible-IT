9b1606957b7db9774b5d3f0e99716f53327d7657
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-04-30 18:14:06 +0200

    Button styling, JS menu-buttons improved

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-const navButton = document.querySelector('.nav-button')
+// Toggle all menu buttons
+const menuButtons = document.querySelectorAll('.menu-button')
+menuButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('menu-button-opened')
+    })
+})
 
+// Toggle main nav
+const navButton = document.querySelector('.nav-button')
 navButton.addEventListener('click', () => {
     const header = document.querySelector('.main-header')
     header.classList.toggle('nav-opened')
