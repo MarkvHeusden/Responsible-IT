99ca769c3326a506308b9311a5e72d01e7686142
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-06-30 16:47:31 +0200

    Added 'Reduce motion' preference

--- js/loadPreferences.js
+++ js/loadPreferences.js
@@ -12,8 +12,8 @@ if (localStorage.getItem('darktheme')) {
 } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     root.setAttribute('data-darktheme', 'true')
 }
-// Get filter preferences
-const storedPreferences = ['grayscale', 'highcontrast', 'hideartwork']
+// Get other preferences
+const storedPreferences = ['grayscale', 'highcontrast', 'hideartwork', 'reducemotion']
 storedPreferences.forEach(preference => {
     const preferenceValue = localStorage.getItem(preference)
     if (preferenceValue) root.setAttribute(`data-${preference}`, preferenceValue)
